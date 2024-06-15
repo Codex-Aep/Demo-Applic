828ef8675d3da96d34400e7164728e49b0f41b5a
Codex-Aep <honeyxep2@gmail.com>
2024-06-15 02:42:49 -0700

    Save Project

---
+++ app/src/main/java/com/example/adminpanel/ModelClasses/ModelClassXd1.java
@@ -0,0 +1,29 @@
+//package com.example.adminpanel.ModelClasses;
+//
+//public class ModelClassXd1 {
+//
+//    String Name;
+//
+//    String Size;
+//
+//    public ModelClassXd1(String name, String size) {
+//        Name = name;
+//        Size = size;
+//    }
+//
+//    public String getName() {
+//        return Name;
+//    }
+//
+//    public void setName(String name) {
+//        Name = name;
+//    }
+//
+//    public String getSize() {
+//        return Size;
+//    }
+//
+//    public void setSize(String size) {
+//        Size = size;
+//    }
+//}
